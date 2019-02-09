a3759485dbedc86374d62c4200b506489156971c
Anthony <zamora.anthony82913@gmail.com>
2019-02-09 15:06:52 -0500

    Add files via upload

---
+++ LPQ.cpp
@@ -0,0 +1,38 @@
+#include "LPQ.h"
+
+template <typename E, typename C>		            // number of elements
+int ListPriorityQueue<E,C>::size() const
+{
+    return L.size();
+}
+
+template <typename E, typename C>		            // is the queue empty?
+bool ListPriorityQueue<E,C>::empty() const
+{
+    return L.empty();
+}
+
+template <typename E, typename C>			        // minimum element
+const E& ListPriorityQueue<E,C>::min() const
+{
+    return L.front();                               // minimum is at the front
+}
+
+template <typename E, typename C>			        // remove minimum
+void ListPriorityQueue<E,C>::removeMin()
+{
+    L.pop_front();
+}
+
+template <typename E, typename C>                   // insert element
+void ListPriorityQueue<E,C>::insert(const E& e)
+{
+    typename std::list<E>::iterator p;
+    p = L.begin();
+
+    while (p != L.end() && !isLess(e, *p))          // find larger element
+    {
+        ++p;
+    }
+    L.insert(p, e);					                // insert e before p
+}
---
+++ main_pq.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cmath>
+#include "LPQ.cpp"
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+
+class Point2D
+{
+private:
+    double x, y;
+    
+public:
+    Point2D(double xcoord, double ycoord)
+    {
+        x = xcoord;
+        y = ycoord;
+    }
+    
+    double getX() const { return x; }
+    double getY() const { return y; }
+    
+    friend ostream& operator<<(ostream& out, const Point2D& q)
+    {
+        out << q.getX() << " " << q.getY();
+        return out;
+    }
+};
+
+class LeftRight
+{
+public:
+    bool operator()(const Point2D& p, const Point2D& q) const
+    {
+        return p.getX() < q.getX();
+    }
+};
+
+class OriginDistance
+{
+public:
+    
+    bool operator()(const Point2D& p) const
+    {
+        return sqrt(pow(p.getX(), 2) + pow(p.getY(), 2));
+    }
+};
+
+int main(int argc, char** argv)
+{
+    OriginDistance od;
+    
+    ListPriorityQueue<Point2D, LeftRight> PList;
+    PList.insert(Point2D(2,5));
+    PList.insert(Point2D(3,2));
+    PList.insert(Point2D(1,9));
+
+    while (!PList.empty())
+    {
+        cout << PList.min() << endl;
+        cout<<"Distance from Origin: "<<od(PList.min())<<endl;
+        PList.removeMin();
+    }
+}
